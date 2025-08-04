3c8dfff7ac6323ef9333cdfc2f1484c4d994ff55
sanehakaur77 <snehaswali9988@gmail.com>
2025-08-04 20:57:42 +0530

    Intial commit

---
+++ Bitmanuplation/1.java
@@ -0,0 +1,12 @@
+class 1{
+    public static void main(String[] args) {
+        //  check if the number is the odd or even
+         int num=12;
+         if((num&1)==1){
+            System.out.println("odd");
+         }
+         else{
+            System.out.println("even");
+         }
+    }
+}
