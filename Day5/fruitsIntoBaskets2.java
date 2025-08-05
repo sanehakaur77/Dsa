e7eb89f574eba65b56a49b4b58b4b4ee96cc5e65
sanehakaur77 <snehaswali9988@gmail.com>
2025-08-05 21:13:26 +0530

    Intial commit

---
+++ Day5/fruitsIntoBaskets2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+       int count=0;
+       for(int i=0;i<fruits.length;i++){
+        for(int j=0;j<baskets.length;j++){
+            if(baskets[j]>=fruits[i]){
+                count++;
+                baskets[j]=0;
+                break;
+            }
+        }
+       } 
+       return fruits.length-count;
+    }
+}
